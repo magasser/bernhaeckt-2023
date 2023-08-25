a75dd3f3259c18d0ba9330af320ee61abaa3ae68
Frey Tim <tfr@ti8m.ch>
2023-08-25 23:26:52 +0200

    added spring project

---
+++ java/src/main/java/ch/planval/datavisualizer/DatavisualizerApplication.java
@@ -0,0 +1,13 @@
+package ch.planval.datavisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatavisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatavisualizerApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/ch/planval/datavisualizer/DatavisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.planval.datavisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatavisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
