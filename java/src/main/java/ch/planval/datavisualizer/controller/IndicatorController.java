06e2f727431a650564d9240a2d61e999424ee9d5
Julian Haldimann <julian@haldimann.be>
2023-08-26 09:40:52 +0200

    Add method to save indicators

--- java/src/main/java/ch/planval/datavisualizer/controller/IndicatorController.java
+++ java/src/main/java/ch/planval/datavisualizer/controller/IndicatorController.java
@@ -3,10 +3,14 @@ package ch.planval.datavisualizer.controller;
 import ch.planval.datavisualizer.model.Indicator;
 import ch.planval.datavisualizer.service.IndicatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.UUID;
 
 public interface IndicatorController {
@@ -16,4 +20,9 @@ public interface IndicatorController {
     @RequestMapping(path = "/indicators/{id}", method = RequestMethod.GET)
     @GetMapping("/")
     Indicator getIndicatorById(@PathVariable(name="id") UUID id);
+
+    @RequestMapping(path = "/indicators", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/")
+    void saveIndicator(@RequestBody Indicator indicator);
+
 }
